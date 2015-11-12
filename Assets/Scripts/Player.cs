9275cbdb3933093b5105458d7b8cfbeb641e6031
Magicolo <straty91@hotmail.com>
2015-11-12 16:53:38 -0500

    player motion

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Pseudo;
 
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+public class Player : PMonoBehaviour
+{
+	[Serializable]
+	public class PlayerStats
+	{
+		public float MovementSpeed = 50f;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public PlayerStats Stats;
+	public TimeComponent CachedTime { get { return cachedTime; } }
+	public Rigidbody2D CachedRigidbody { get { return cachedRigidbody; } }
+
+	CachedValue<TimeComponent> cachedTime;
+	CachedValue<Rigidbody2D> cachedRigidbody;
+	Vector2 inputDirection;
+
+	public Player()
+	{
+		cachedTime = new CachedValue<TimeComponent>(GetComponent<TimeComponent>);
+		cachedRigidbody = new CachedValue<Rigidbody2D>(GetComponent<Rigidbody2D>);
+	}
+
+	void Update()
+	{
+		UpdateInput();
+	}
+
+	void FixedUpdate()
+	{
+		UpdateMotion();
+	}
+
+	void UpdateInput()
+	{
+		inputDirection.x = InputManager.Instance.GetAxis(InputManager.Players.Player1, "MoveX");
+		inputDirection.y = InputManager.Instance.GetAxis(InputManager.Players.Player1, "MoveY");
+		inputDirection = inputDirection.ClampMagnitude(0f, 1f);
+	}
+
+	void UpdateMotion()
+	{
+		CachedRigidbody.AddForce(inputDirection * Stats.MovementSpeed);
 	}
 }
