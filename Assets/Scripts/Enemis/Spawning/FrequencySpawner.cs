fc386f2e9ee26902344fbe7b578476f3fd5d7d9c
richard <rail.rich@gmail.com>
2015-11-13 15:24:57 -0500

    BaseSpawnner that does nothing for now

---
+++ Assets/Scripts/Enemis/Spawning/FrequencySpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pseudo;
+
+public class FrequencySpawner : MonoBehaviour {
+
+    public Enemy EnemyToSpawn;
+    public MinMax spawningFrequency;
+
+    [Disable]public float nextSpawn;
+	
+	void Start () {
+        nextSpawn = spawningFrequency.GetRandom(ProbabilityDistributions.Normal);
+    }
+	
+	void Update () {
+        nextSpawn -= TimeManager.Enemy.DeltaTime;
+        if (nextSpawn <= 0) 
+        {
+
+        }
+	}
+}
