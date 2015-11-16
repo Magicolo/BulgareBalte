1032d91623d245e5dde9e2661fe455d60ca06a49
Magicolo <straty91@hotmail.com>
2015-11-16 10:42:36 -0500

    Health bar in tha worx.

---
+++ Assets/Scripts/UI/UIBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Pseudo;
+using UnityEngine.UI;
+
+public class UIBar : PMonoBehaviour
+{
+	public Slider Slider;
+	public CanvasGroup Fader;
+	public float FadeSpeed = 5f;
+
+	public float Value { get { return Slider.value; } set { Slider.value = value; } }
+	public bool Show { get; set; }
+
+	protected virtual void Update()
+	{
+		Fader.alpha = Mathf.Lerp(Fader.alpha, Show ? 1f : 0f, TimeManager.UI.DeltaTime);
+	}
+}
---
+++ Assets/Scripts/UI/UIHealthBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Pseudo;
+
+public class UIHealthBar : UIBar
+{
+	public float HideDelay = 3f;
+
+	CharacterBase character;
+	float lastValue;
+	float lastValueChangeTime;
+
+	public virtual void Initialize(CharacterBase character)
+	{
+		this.character = character;
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+
+		CachedTransform.position = character.CachedTransform.position;
+		Show = TimeManager.UI.Time > lastValueChangeTime + HideDelay;
+
+		float value = character.CurrentStats.Health / character.Stats.Health;
+
+		if (lastValue != value)
+		{
+			lastValue = value;
+			lastValueChangeTime = TimeManager.UI.Time;
+		}
+	}
+}
