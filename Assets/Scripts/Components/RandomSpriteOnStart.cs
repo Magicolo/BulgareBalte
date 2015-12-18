e9ec500d63b08ad0492793f6c5f74c71d181ce00
richard <rail.rich@gmail.com>
2015-12-18 11:36:25 -0500

    Spectator Groups

---
+++ Assets/Scripts/Components/RandomSpriteOnStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Pseudo;
+
+public class RandomSpriteOnStart : PComponent
+{
+
+	public SpriteRenderer SpriteRenderer;
+	public Sprite[] Sprites;
+
+	protected override void Start()
+	{
+		SpriteRenderer.sprite = Sprites.GetRandom();
+		this.Destroy();
+	}
+
+}
---
+++ Assets/Scripts/General/InstatiatePrefabInZone.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pseudo;
+
+public class InstatiatePrefabInZone : MonoBehaviour
+{
+
+	public float Chance;
+	public GameObject Prefab;
+	public Zone2DBase Zone;
+
+	void Start()
+	{
+		if (Random.Range(0f, 1f) <= Chance)
+		{
+			GameObject newGO = GameObject.Instantiate(Prefab);
+			newGO.transform.parent = this.transform;
+			newGO.transform.localPosition = Zone.GetRandomLocalPoint();
+		}
+
+		this.Destroy();
+	}
+}
