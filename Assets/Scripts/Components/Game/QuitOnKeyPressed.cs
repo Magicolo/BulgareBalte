e13139411c64bbd54b7c979aaeaa42229e1837a0
Richard <Rail.rich@gmail.com>
2016-01-05 18:49:28 -0500

    GameManager creadit

---
+++ Assets/Scripts/Components/Game/QuitOnKeyPressed.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Pseudo;
+using System;
+
+[Serializable]
+public class QuitOnKeyPressed : ComponentBase, IUpdateable
+{
+	public float UpdateRate { get { return 0; } }
+
+	public KeyCode QuitKey;
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(QuitKey))
+			Application.Quit();
+	}
+}
+
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using Pseudo;
+
+public class GameManager : Singleton<GameManager>
+{
+
+	public enum GameStates { None, Playing, Loading, Winning, Losing };
+	[Disable]
+	public GameStates CurrentGameState;
+
+	public string[] LevelNames;
+
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+
+	void Update()
+	{
+
+	}
+}
