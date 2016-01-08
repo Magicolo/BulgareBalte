d9e71442cf7f9c319c3115109a1a7f10a05a6aee
Richard <Rail.rich@gmail.com>
2016-01-08 16:48:42 -0500

    a

---
+++ Assets/Scripts/Components/Graphics/OscillateColorComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Pseudo;
+using System;
+
+[Serializable, ComponentCategory("Graphics"), RequireComponent(typeof(TimeComponent))]
+public class OscillateColorComponent : ComponentBase, IUpdateable
+{
+	public Renderer Renderer;
+	public float Frequency;
+	public float Amplitude;
+	public float Center;
+	public Channels Channels = Channels.RGBA;
+
+	float t;
+
+	public float UpdateRate { get { return 0; } }
+
+	public void Update()
+	{
+		var time = Entity.GetComponent<TimeComponent>();
+		t += time.DeltaTime;
+		Renderer.OscillateColor(Frequency, Amplitude, Center, t, false, Channels);
+	}
+}
+
